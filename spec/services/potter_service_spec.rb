fc5f6222df7d0ecbce982c55003143c7b5c0e9af
Kelly Sandeson <kellysandeson@gmail.com>
2019-10-14 11:15:09 -0600

    Change test

--- spec/services/potter_service_spec.rb
+++ spec/services/potter_service_spec.rb
@@ -5,10 +5,12 @@ RSpec.describe "PotterService" do
     it "returns characters data by house " do
 
       service = PotterService.new
-      raw_data = service.get_house_data
+      raw_data = service.get_house_data("/houses")
 
       expect(service).to be_an_instance_of(PotterService)
       expect(raw_data).to be_a Hash
+      expect(raw_data[0]).to have_key :school
+      expect(raw_data[0]).to have_key :_id
     end
   end
 end
