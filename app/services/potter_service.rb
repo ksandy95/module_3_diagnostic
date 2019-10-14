7dea5fe566fea31f4cc48bd15a9e715be2a065ab
Kelly Sandeson <kellysandeson@gmail.com>
2019-10-14 10:30:27 -0600

    Create potter service

---
+++ app/services/potter_service.rb
@@ -0,0 +1,16 @@
+class PotterService
+
+  def get_house_data(url, params)
+    response = conn.get(url, params)
+    parsed_data = JSON.parse(response.body, symbolize_names: true)
+    binding.pry
+  end
+
+  def conn
+    Faraday.new(url: 'https://www.potterapi.com/v1/') do |f|
+      f.adapter Faraday.default_adapter
+      f.params[:key] = ENV['POTTER_API_KEY']
+    end
+  end
+
+end
