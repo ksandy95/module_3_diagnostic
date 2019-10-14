2aed57b5e722abfdb5fef2d02bb7fc9c46129bd5
Kelly Sandeson <kellysandeson@gmail.com>
2019-10-14 10:59:01 -0600

    Create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   get '/search', to: 'search#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :characters, only: [:index, :show]
+      resources :houses, only: [:index, :show]
+    end
+  end
+
+
 end
